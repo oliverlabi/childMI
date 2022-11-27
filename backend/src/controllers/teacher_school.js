ca24d523a0f7d6babf3b02a28a51fbfa1cf4db00
Oliver <olku211@tlu.ee>
2022-11-27 18:37:13 +0200

    Add getSchoolChildren route and controller to backend

--- backend/src/controllers/teacher_school.js
+++ backend/src/controllers/teacher_school.js
@@ -7,11 +7,37 @@ exports.getSchoolTeachers = async (req, res) => {
         const results = await sequelize.query(
             "SELECT " +
                 "ts.teacher_id, " +
-                "CONCAT(t.first_name, ' ' , t.last_name) as full_name, ts.school_id, " +
-                "s.name " +
+                "CONCAT(t.first_name, ' ' , t.last_name) as teacher_full_name, " +
+                "ts.school_id, " +
+                "s.name AS school_name " +
             "FROM teacher_school ts " +
             "INNER JOIN teacher t ON ts.teacher_id = t.id " +
             "INNER JOIN school s ON ts.school_id = s.id " +
+            "WHERE s.id = ?;",
+            {
+                replacements: [id],
+                type: QueryTypes.SELECT
+            }
+        )
+        return res.status(200).json({ results });
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
+
+exports.getSchoolChildren = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const results = await sequelize.query(
+            "SELECT " +
+                "c.id AS child_id," +
+                "c.name_code, " +
+                "ts.school_id " +
+            "FROM teacher_school ts " +
+            "INNER JOIN teacher t ON ts.teacher_id = t.id " +
+            "INNER JOIN school s on ts.school_id = s.id " +
+            "INNER JOIN teacher_children tc ON t.id = tc.teacher_id " +
+            "INNER JOIN child c ON tc.child_id = c.id " +
             "WHERE s.id = ?;",
             {
                 replacements: [id],
--- backend/src/routes/teacher_school.js
+++ backend/src/routes/teacher_school.js
@@ -1,6 +1,7 @@
 const router = require("express").Router()
 const teacherSchoolController = require("../controllers/teacher_school")
 
-router.get("/:id", teacherSchoolController.getSchoolTeachers)
+router.get("/teacher/:id", teacherSchoolController.getSchoolTeachers)
+router.get("/child/:id", teacherSchoolController.getSchoolChildren)
 
 module.exports = router;
