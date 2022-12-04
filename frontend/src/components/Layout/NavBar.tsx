a2cc6094b663edc764f2d4e18c13d15d14b68799
Oliver <olku211@tlu.ee>
2022-12-04 12:40:36 +0200

    Fix navbar to check first element of pathname for active keys

--- frontend/src/components/Layout/NavBar.tsx
+++ frontend/src/components/Layout/NavBar.tsx
@@ -1,15 +1,19 @@
 import './css/NavBar.scss';
 import { Nav } from 'react-bootstrap';
 import Navbar from 'react-bootstrap/Navbar';
+import {useLocation} from "react-router-dom";
 
 const NavBar = () => {
+    const { pathname } = useLocation();
+    const splitPath = `/${pathname.split("/")[1]}`;
+
     return (
         <>
             <Navbar expand="md">
                 <Navbar.Brand>Lastekeele korpuse haldusliides</Navbar.Brand>
                 <Navbar.Toggle className="navbar-toggle"></Navbar.Toggle>
                 <Navbar.Collapse role={undefined}>
-                    <Nav className="justify-content-end" activeKey={location.pathname}>
+                    <Nav className="justify-content-end" activeKey={splitPath}>
                         <Nav.Link href="/children">Õpilased</Nav.Link>
                         <Nav.Link href="/teachers">Õpetajad</Nav.Link>
                         <Nav.Link href="/schools">Koolid</Nav.Link>
